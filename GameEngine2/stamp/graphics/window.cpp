1b86e2a6c1393b2c8a9c4083f3c3bc6ea6ca7d5b
stamparkour <42157566+stamparkour@users.noreply.github.com>
2025-08-21 21:38:26 -0400

    sfdssd

--- GameEngine2/stamp/graphics/window.cpp
+++ GameEngine2/stamp/graphics/window.cpp
@@ -12,5 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <stamp/graphics/window_base.h>
+#include <stamp/graphics/window.h>
+
+using namespace STAMP_GRAPHICS_NAMESPACE;
+
 
