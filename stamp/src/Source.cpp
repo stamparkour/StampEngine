983904657d1fe18817707f7ffda511358c95ff3a
stamparkour <42157566+stamparkour@users.noreply.github.com>
2026-01-26 10:27:36 -0500

    reset repo with new version of project.

--- stamp/src/Source.cpp
+++ stamp/src/Source.cpp
@@ -1,7 +1,3 @@
 #include <iostream>
 #include <stamp/stamp_config.h>
-
-int main(int argc, char** argv) {
-	std::cout << "Hello World!" << STAMP_VERSION_MAJOR << std::endl;
-	return 0;
-}
+#include <stamp/resource/resource.h>
