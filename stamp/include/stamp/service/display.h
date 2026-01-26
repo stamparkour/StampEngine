902dc01fbbcf8a01836862603c477e363fff36f4
stamparkour <42157566+stamparkour@users.noreply.github.com>
2026-01-26 15:30:42 -0500

    fixed stamp test

---
+++ stamp/include/stamp/service/display.h
@@ -0,0 +1,8 @@
+#ifndef STAMP_SERVICE_DISPLAY_H
+#define STAMP_SERVICE_DISPLAY_H
+
+namespace stamp {
+
+}
+
+#endif // STAMP_SERVICE_DISPLAY_H
--- stamp/include/stamp/service/network.h
+++ stamp/include/stamp/service/network.h
@@ -1,6 +1,8 @@
 #ifndef STAMP_SERVICE_NETWORK_H
 #define STAMP_SERVICE_NETWORK_H
 
+namespace stamp {
 
+}
 
 #endif // STAMP_SERVICE_NETWORK_H
